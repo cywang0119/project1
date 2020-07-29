e69ddc93816f1dd73b4f7732480ee24672d29879
tianchux <bob@example.com>
2020-07-29 15:27:46 -0400

    huai huai fixed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cywang@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tianchuxie@localhost:5432/todoapp'
 db=SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -13,17 +13,18 @@ class Todo(db.Model):
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+# db.create_all()
 
-@app.route('/todos/create', methods=['GET','POST'])
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.getjson()['description']
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-      'description': todo.description
-  })
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description':description
+    })
+
 
 @app.route('/')
 def index():
